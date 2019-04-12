e8c34faeb844d15a23772d5e0ff82a149e9eb98f
Jon Urry <jon@urry.me>
2019-04-12 12:03:39 +0100

    Test hide sold items

--- src/App.test.js
+++ src/App.test.js
@@ -3,7 +3,9 @@ import { fireEvent, render, waitForElement } from 'react-testing-library';
 import ReactDOM from 'react-dom';
 import App from './App';
 import API from './api';
+import { toMatchDiffSnapshot } from 'snapshot-diff';
 
+expect.extend({ toMatchDiffSnapshot });
 jest.mock('./api');
 
 it('renders without crashing', () => {
@@ -19,9 +21,18 @@ it('should match App Snapshot', async () => {
 });
 
 it('should like a product', async () => {
-  const { getByTestId } = await render(<App api={new API()} />);
+  const { getByTestId, getByText } = await render(<App api={new API()} />);
+  await waitForElement(() => getByText('Polo Shirt'));
   const likeButton = getByTestId('like-button');
   expect(likeButton.className).not.toMatch(/liked/);
   fireEvent.click(likeButton);
   expect(likeButton.className).toMatch(/liked/);
+});
+
+it('should hide sold items', async () => {
+  const { asFragment, getByText } = await render(<App api={new API()} />);
+  await waitForElement(() => getByText('Polo Shirt'));
+  const firstRender = asFragment();
+  fireEvent.click(getByText(/Hide Sold Items/));
+  expect(firstRender).toMatchDiffSnapshot(asFragment());
 });
